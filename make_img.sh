91d2fdc2707eb259bd6b39c70e61f87ae497d1b1
haoxingxing <mr.haoxx@gmail.com>
2019-03-10 04:34:52 +0000

    AD

--- make_img.sh
+++ make_img.sh
@@ -12,11 +12,11 @@ cd ..
 echo  -e "\033[36m  Setting up drivers\033[0m"
 sudo cp -b /etc/resolv.conf ubuntu-mount/etc/resolv.conf
 sudo rm ubuntu-mount/etc/apt/sources.list
-sudo cp ./sources.list ubuntu-mount/etc/apt/sources.list
-sudo cp -b -r ./system ubuntu-mount/
-sudo cp -b -r ./lib ubuntu-mount/
+sudo cp ./drivers/sources.list ubuntu-mount/etc/apt/sources.list
+sudo cp -b -r ./drivers/system ubuntu-mount/
+sudo cp -b -r ./drivers/lib ubuntu-mount/
 echo -e  "\033[36m  Changing Root\033[0m"
-cat <<EOF | sudo bash ch-mount.sh -m ubuntu-mount/
+cat <<EOF | sudo bash ./tools/ch-mount.sh -m ubuntu-mount/
 echo -e  "\033[36m  Setting hostname\033[0m"
 echo "starpi">/etc/hostname
 echo "127.0.0.1 localhost">>/etc/hosts
@@ -37,17 +37,13 @@ y
 y
 y
 echo -e  "\033[36m  Installing Packages:base\033[0m"
-apt-get install -y language-pack-en-base ntp sudo ssh net-tools ethtool wireless-tools ifupdown network-manager iputils-pingbash-completion htop synaptic alsa-utils rsyslog nano vim git udev build-essential blueman sshfs openssh-server
+apt-get install -y  language-pack-en-base screen ntp sudo ssh net-tools ethtool pkg-config wireless-tools ifupdown network-manager iputils-ping bash-completion htop synaptic alsa-utils nano vim git udev build-essential sshfs openssh-server telnetd bluez telnet nmon curl language-pack-zh
 echo -e  "\033[36m  Installing Packages:video\033[0m"
 apt-get install -y gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-good  gstreamer1.0-plugins-bad alsa-utils
 echo -e  "\033[36m  Installing Packages:check\033[0m"
 apt-get install -f -y
 echo  -e "\033[36m  Installing Packages:clean\033[0m"
-rm -rf /var/lib/apt/lists/*
-rm -rf /var/cache/*
-chmod -R +x /bin/* /sbin/* /lib/* /usr/share/* /usr/local/share/* /usr/bin/* /usr/sbin/* /usr/lib/* /usr/local/bin/* /usr/local/lib/* /usr/local/sbin/*
-chmod -R -x /lib/systemd/system
-chmod 4755 'which sudo' 'which passwd' 'which su' 'which chsh' 'which gpasswd' 'which mount' 'which umount' 'which chfn' 'which newgrp'
+rm -rf /var/lib/apt/lists/* /var/cache/man/*
 echo  -e "\033[36m  Setting users\033[0m"
 useradd -s '/bin/bash' -m -G adm,sudo $1
 passwd $1
@@ -60,10 +56,10 @@ echo  -e "\033[36m  Done\033[0m"
 exit
 EOF
 echo  -e "\033[36m  Umounting\033[0m"
-sudo bash ch-mount.sh -u ubuntu-mount/
-sudo rm -rf ubuntu-mount/dev/* ubuntu-mount/proc/* ubuntu-mount/sys/* ubuntu-mount/dev/pts/*
+sudo bash ./tools/ch-mount.sh -u ubuntu-mount/
+rm -Rf ubuntu-mount/dev/* ubuntu-mount/run/*
 echo  -e "\033[36m  Packing\033[0m"
-sudo ./make_ext4fs -s -l3072M -a root -L rootfs rootfs.img ubuntu-mount
+sudo ./tools/qemu-x86_64 -L ./tools/make_ext4fs -s -l3072M -a root -L rootfs rootfs.img ubuntu-mount
 echo  -e "\033[36m  Package has save to rootfs.img\033[0m"
 sudo umount ubuntu.img
 
