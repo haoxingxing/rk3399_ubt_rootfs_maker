6bd135d11719a8a12e13f04848384e1947ab56af
haoxingxing <mr.haoxx@gmail.com>
2019-03-10 05:49:18 +0000

    A fix

--- make_img.sh
+++ make_img.sh
@@ -1,9 +1,9 @@
 echo -e "\033[36m  Creating Image\033[0m"
-dd if=/dev/zero of=ubuntu.img bs=1M count=3072
-sudo mkfs.ext4 ubuntu.img
+#dd if=/dev/zero of=ubuntu.img bs=1M count=3072
+#sudo mkfs.ext4 ubuntu.img
 echo -e "\033[36m  Mounting Image\033[0m"
 mkdir ubuntu-mount
-sudo mount ubuntu.img ubuntu-mount
+#sudo mount ubuntu.img ubuntu-mount
 echo -e  "\033[36m  Getting ubuntu rootfs base\033[0m"
 wget -c https://mirrors.tuna.tsinghua.edu.cn/ubuntu-cdimage/ubuntu-base/releases/18.04.2/release/ubuntu-base-18.04.2-base-arm64.tar.gz -O base.tar.gz
 cd ubuntu-mount
@@ -33,11 +33,10 @@ y
 y
 y
 y
-y
-y
-y
+EOF
+cat <<EOF | sudo chroot ubuntu-mount/
 echo -e  "\033[36m  Installing Packages:base\033[0m"
-apt-get install -y  language-pack-en-base screen ntp sudo ssh net-tools ethtool pkg-config wireless-tools ifupdown network-manager iputils-ping bash-completion htop synaptic alsa-utils nano vim git udev build-essential sshfs openssh-server telnetd bluez telnet nmon curl language-pack-zh
+apt-get install -y  language-pack-en-base screen ntp sudo ssh net-tools ethtool pkg-config wireless-tools ifupdown network-manager iputils-ping bash-completion htop synaptic alsa-utils nano vim git udev build-essential sshfs openssh-server telnetd bluez telnet nmon curl language-pack-zh-hans
 echo -e  "\033[36m  Installing Packages:video\033[0m"
 apt-get install -y gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-good  gstreamer1.0-plugins-bad alsa-utils
 echo -e  "\033[36m  Installing Packages:check\033[0m"
@@ -59,9 +58,9 @@ echo  -e "\033[36m  Umounting\033[0m"
 sudo bash ./tools/ch-mount.sh -u ubuntu-mount/
 rm -Rf ubuntu-mount/dev/* ubuntu-mount/run/*
 echo  -e "\033[36m  Packing\033[0m"
-sudo ./tools/qemu-x86_64 -L ./tools/make_ext4fs -s -l3072M -a root -L rootfs rootfs.img ubuntu-mount
+sudo make_ext4fs -s -l3072M -a root -L rootfs rootfs.img ubuntu-mount
 echo  -e "\033[36m  Package has save to rootfs.img\033[0m"
-sudo umount ubuntu.img
+#sudo umount ubuntu.img
 
 
 
